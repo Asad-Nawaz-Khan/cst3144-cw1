c0290339ed70f60370c2d25ceeed7b8646fe111e
Asad Nawaz Khan <asadnawazkhann@gmail.com>
2024-10-29 16:02:42 +0400

    Added Intial Files

---
+++ products.js
@@ -0,0 +1,56 @@
+let products =[
+    {
+    "id":1001,
+    "title":"cat food 25lb bag",
+    "description":"A 25 pounds bg of <em>irresistable</em>,organic goodness for you",
+    "price":20.00,
+    "image":"images/download.jpg",
+    "availableInventory":10,
+    "rating":2
+    },
+    {
+        "id":1002,
+    "title":"cat food 25lb bag",
+    "description":"A 25 pounds bg of <em>irresistable</em>,organic goodness for you",
+    "price":30.00,
+    "image":"images/galaxy.jpg",
+    "availableInventory":10,
+    "rating":2
+    },
+    {
+        "id":1003,
+    "title":"cat food 25lb bag",
+    "description":"A 25 pounds bg of <em>irresistable</em>,organic goodness for you",
+    "price":5.00,
+    "image":"images/download.jpg",
+    "availableInventory":10,
+    "rating":2
+    },
+    {
+        "id":1004,
+    "title":"cat food 25lb bag",
+    "description":"A 25 pounds bg of <em>irresistable</em>,organic goodness for you",
+    "price":20.00,
+    "image":"images/download.jpg",
+    "availableInventory":10,
+    "rating":2
+    },
+    {
+        "id":1005,
+    "title":"cat food 25lb bag",
+    "description":"A 25 pounds bg of <em>irresistable</em>,organic goodness for you",
+    "price":20.00,
+    "image":"images/download.jpg",
+    "availableInventory":10,
+    "rating":2
+    },
+    {
+    "id":1006,
+    "title":"cat food 25lb bag",
+    "description":"A 25 pounds bg of <em>irresistable</em>,organic goodness for you",
+    "price":20.00,
+    "image":"images/download.jpg",
+    "availableInventory":10,
+    "rating":2
+    }
+]
